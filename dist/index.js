a744cbcd28430a641341427201327830ad7a42d4
shadab-k <shadabpp27@gmail.com>
2023-11-27 17:01:49 +0530

    build fixes

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require('express');
+const connectToMongo = require('./server');
+const app = express();
+let cors = require("cors");
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cors());
+//  app.use(bodyParser.json());
+
+// Routes
+app.use('/data', require('./routes/create-data'));
+connectToMongo();
